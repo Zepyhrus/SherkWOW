0e6dee63b8e4cb5cf9ff7c9792ad7ce893b1c94b
Zepyhrus <sherkfung@gmail.com>
2023-02-03 11:02:51 +0800

    sherk to sherkWOW

---
+++ SherkWOW.lua
@@ -0,0 +1,36 @@
+local zone = nil
+local TimeSinceLastUpdate = 0
+
+local function UpdatSherkWOW(self, elapsed)
+  if zone ~= GetRealZoneText() then
+	  zone = GetRealZoneText()
+  	SetMapToCurrentZone()
+  end
+  
+  TimeSinceLastUpdate = TimeSinceLastUpdate + elapsed
+ 	
+  if TimeSinceLastUpdate > .5 then	
+    TimeSinceLastUpdate = 0
+    local posX, posY = GetPlayerMapPosition("player");
+    local x = math.floor(posX * 10000)/100
+    local y = math.floor(posY * 10000)/100
+    SherkWOWFontString:SetText("|c98FB98ff("..x..", "..y..")")	
+ 	end	
+end
+ 
+function SherkWOW_OnLoad(self, event,...) 
+  self:RegisterEvent("ADDON_LOADED")	
+end
+
+function SherkWOW_OnEvent(self, event, ...) 
+  if event == "ADDON_LOADED" and ... == "SherkWOW" then
+    self:UnregisterEvent("ADDON_LOADED")		
+    SherkWOW:SetSize(100, 50)
+    SherkWOW:SetPoint("TOP", "Minimap", "BOTTOM", 5, -5)
+    SherkWOW:SetScript("OnUpdate", UpdatSherkWOW)
+    local coordsFont =    SherkWOW:CreateFontString("SherkWOWFontString", "ARTWORK", "GameFontNormal")
+    coordsFont:SetPoint("CENTER", "SherkWOW", "CENTER", 0, 0)
+    coordsFont:Show()
+    SherkWOW:Show()		
+  end
+end
