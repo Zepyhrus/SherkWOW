8ab5a355f6977ff13ae145a99daaf5dfefbb038a
Zepyhrus <sherkfung@gmail.com>
2023-02-17 03:05:07 +0800

    AP in the middle

--- SherkWOW.lua
+++ SherkWOW.lua
@@ -20,17 +20,19 @@ local function UpdatSherkWOW(self, elapsed)
  	
   if TimeSinceLastUpdate > .5 then	
     TimeSinceLastUpdate = 0
-    -- local posX, posY = GetPlayerMapPosition("player");
-    -- local x = math.floor(posX * 10000)/100
-    -- local y = math.floor(posY * 10000)/100
+    local posX, posY = GetPlayerMapPosition("player");
+    local x = math.floor(posX * 10000)/100;
+    local y = math.floor(posY * 10000)/100;
 
     local base, posBuff, negBuff = UnitAttackPower("player");
     local effective = math.floor((base + posBuff + negBuff));
 
-    local mainSpeed, offSpeed = UnitAttackSpeed("player");
-    mainSpeed = math.floor(mainSpeed * 100000);
+    -- local mainSpeed, offSpeed = UnitAttackSpeed("player");
+    -- mainSpeed = math.floor(mainSpeed*100);
     
-    SherkWOWFontString:SetText("|cffffffff"..effective.."\n"..mainSpeed.."")
+    SherkWOWFontString:SetText("|cffffffff"..effective.."")
+
+    -- print(""..x..", "..y.."") 
  	end	
 end
  
@@ -43,14 +45,16 @@ function SherkWOW_OnEvent(self, event, ...)
   if event == "ADDON_LOADED" and ... == "SherkWOW" then
     self:UnregisterEvent("ADDON_LOADED")		
     SherkWOW:SetSize(200, 500)
-    SherkWOW:SetPoint("TOPLEFT", 0, 0)
+    SherkWOW:SetPoint("CENTER", 0, 0)
     SherkWOW:SetScript("OnUpdate", UpdatSherkWOW)
 
     local coordsFont = SherkWOW:CreateFontString("SherkWOWFontString", "ARTWORK", "GameFontNormal")
-    local Path, Size, Flags = coordsFont:GetFont()
-    coordsFont:SetFont(Path, 18, Flags)
-    coordsFont:SetPoint("TOPLEFT", 5, -5)
-    -- coordsFont:SetSize(100, 40)
+    -- local Path, Size, Flags = coordsFont:GetFont()
+    -- coordsFont:SetFont(Path, 18, Flags)
+    coordsFont:SetPoint("CENTER", "SherkWOW", 0, -60)
+    -- coordsFont:SetJustifyH("CENTER")
+    -- coordsFont:SetJustifyH("MIDDLE")
+
     coordsFont:Show()
     SherkWOW:Show()		
   end
